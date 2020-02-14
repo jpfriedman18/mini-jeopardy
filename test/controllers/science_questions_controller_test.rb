66b33ff972064b315cd9a5a3e50adb8a09af1876
jpfriedman18 <jpfriedman18@gmail.com>
2020-02-14 14:18:18 -0800

    Exclude ActiveRecord from config and remove unnecessary files to be able to run tests

--- test/controllers/science_questions_controller_test.rb
+++ test/controllers/science_questions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class ScienceQuestionsControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get science_questions_index_url
+    get science_questions_url
     assert_response :success
   end
 
