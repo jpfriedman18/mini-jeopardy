b8839dbd90418b8914cab85425c1a86522b8cb1d
jpfriedman18 <jpfriedman18@gmail.com>
2020-02-14 14:49:35 -0800

    Add/config VCR for easier stubbing in tests

--- test/controllers/science_questions_controller_test.rb
+++ test/controllers/science_questions_controller_test.rb
@@ -1,9 +1,12 @@
 require 'test_helper'
 
 class ScienceQuestionsControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
-    get science_questions_url
-    assert_response :success
-  end
+  
+    test "should get index" do
+      VCR.use_cassette("science_questions") do
+        get science_questions_url
+        assert_response :success
+      end
+    end
 
 end
